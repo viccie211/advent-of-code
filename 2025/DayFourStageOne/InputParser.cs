f963c654473519034017e4d32c4b2df02d1948d6
Victor Remmerswaal <vremmerswaal@auxilium.nl>
2025-12-05 21:22:29 +0100

    Started work on Day Four

---
+++ 2025/DayFourStageOne/InputParser.cs
@@ -0,0 +1,13 @@
+namespace DayFourStageOne;
+
+public class InputParser
+{
+    public int[,] ParseInput(string inputString)
+    {
+        var split = inputString.Split(Environment.NewLine);
+        foreach (var line in split)
+        {
+            
+        }
+    }
+}
