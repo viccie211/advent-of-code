54b561520a3f19ecb7a133c39c55e6d1921ab6f0
Victor Remmerswaal <vremmerswaal@auxilium.nl>
2025-12-05 19:46:03 +0100

    Gold Star on Day Two Stage Two!

--- 2025/DayTwoStageTwo/IdRange.cs
+++ 2025/DayTwoStageTwo/IdRange.cs
@@ -10,7 +10,30 @@ public class IdRange
     {
         for (long i = Min; i <= Max; i++)
         {
+            var chunkOptions = new List<int>();
             var stringId = i.ToString();
+            for (int j = stringId.Length / 2; j > 0; j--)
+            {
+                if (stringId.Length % j == 0)
+                {
+                    chunkOptions.Add(j);
+                }
+            }
+
+            foreach (var chunkOption in chunkOptions)
+            {
+                var chunks = new List<string>();
+                for (int j = 0; j < stringId.Length/chunkOption; j++)
+                {
+                    chunks.Add(stringId.Substring(j*chunkOption, chunkOption));
+                }
+
+                if (chunks.All(c => c == chunks[0]))
+                {
+                    InvalidIds.Add(i);
+                    break;
+                }
+            }
         }
     }
 }
